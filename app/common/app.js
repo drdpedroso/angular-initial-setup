777307736e0b358a8147b2a1ebe72a549fa77a2e
Eduardo Pedroso <eduardo.pedroso@matera.com>
2016-07-16 16:59:54 -0300

    Adding view

--- app/common/app.js
+++ app/common/app.js
@@ -1,13 +1,19 @@
-'use strict';
+(function () {
+  'use strict';
 
-var app = angular.module('angular-mdl-skeleton', ['ui.router']);
+  var app = angular.module('angular-mdl-skeleton', ['ui.router']);
 
-// app.config(function($stateProvider) {
-//
-//   $stateProvider
-//     .state('test-route',{
-//       url: '/',
-//       templateUrl:"templates/test.html"
-//     });
-//
-// });
+  app.config(function($stateProvider) {
+
+    $stateProvider
+      .state('example',{
+        url: '/',
+        views: {
+          'content': {
+            templateUrl: './modules/example/views/example.html'
+          }
+        }
+      });
+
+  });
+}());
