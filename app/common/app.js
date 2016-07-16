51c6cf57237e83dff7ef002699fbfc975837925e
Eduardo Pedroso <eduardo.pedroso@matera.com>
2016-07-16 13:41:59 -0300

    Setting up angular

---
+++ app/common/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = angular.module('angular-mdl-skeleton', []);
+
+// app.config(function($stateProvider) {
+//
+//   $stateProvider
+//     .state('test-route',{
+//       url: '/',
+//       templateUrl:"templates/test.html"
+//     });
+//
+// });
